7eca019feef8e96d74d7d5c00e8482b2f4fd4476
alensa97 <dzaferspahic.alen@gmail.com>
2025-07-15 15:47:29 +0200

    check credentials

--- src/test/java/com/appiumProject/tests/FirstTest.java
+++ src/test/java/com/appiumProject/tests/FirstTest.java
@@ -1,11 +1,8 @@
 package com.appiumProject.tests;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.appiumProject.base.BaseTest;
-import com.appiumProject.base.DriverFactory;
 
 public class FirstTest extends BaseTest{
     
@@ -13,8 +10,5 @@ public class FirstTest extends BaseTest{
     public void test() throws InterruptedException {
         System.out.println("Emulator is running");
         Thread.sleep(5000);
-        String text = DriverFactory.getDriver().findElement(By.id("login_screen_welcome_back_label")).getAttribute("content-desc");
-        System.out.println(text);
-        Assert.assertEquals(text, "Welcome Back");
     }
 }
